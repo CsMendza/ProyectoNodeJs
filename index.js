9e6e3cfec7adcd233f59fe9bba51f356ac18843b
Antonio Iván García Galván <agarcia261@alumnos.uaq.mx>
2023-05-30 13:28:53 -0600

    Hello

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const express = require(express)
+const express = require('express')
 const app = express();
+
+app.get("/",(req, res, next)=>{
+    res.status(200);
+    res.send("Bienvenido");
+})
 
 app.listen(3000, () => {
     console.log("Server is running...");
