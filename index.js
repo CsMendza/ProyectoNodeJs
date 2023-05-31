59c6df200b451f21ea29031b599faeef55c2ba23
Antonio Iván García Galván <agarcia261@alumnos.uaq.mx>
2023-05-31 08:52:50 -0600

    nodemonxd

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const app = express();
+const db = require('./config/database');
+
 
 app.get("/",(req, res, next)=>{
     res.status(200);
     res.send("Bienvenido");
 })
 
-app.listen(3000, () => {
+
+
+
+app.get("/:consultas", (req, res, next)=>{
+    console.log(req.params.consultas);
+    res.status(200);
+    res.send("Hola "+ req.params.consultas);
+})
+
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running...");
 });
